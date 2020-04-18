5baeead52c65feff55197ccde5fc8e2cb76c938d
axel.legue <axel.legue31@gmail.com>
2020-04-18 02:17:03 +0200

    feat: dice layout created 	- add Expanded widget 	- add FlatButton with his void call back

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,29 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){ // anonymous function
+                print('Left button got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){ // anonymous function
+                print('Right button got pressed');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
